63df692145f6d4ebffd6b0c76819c7d74af2105d
Bikash Rai <bkr24snj@bangor.ac.uk>
2024-11-01 19:47:23 +0000

    Assignment WK5 complete

--- behavior_trends_analysis.py
+++ behavior_trends_analysis.py
@@ -29,11 +29,11 @@ loyal_df = loyalty_customers(df, min_purchases)
 print(loyal_df.head())
 
 def quarterly_revenue(df):
-    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
-    df['Quarter'] = df['InvoiceDate'].dt.to_period('Q')
-    df['Revenue'] = df['Quantity'] * df['UnitPrice']
-    total_revenue = df.groupby('Quarter')['Revenue'].sum().reset_index()
-    total_revenue.columns = ['Quarter', 'Total Revenue']
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+    df["Quarter"] = df["InvoiceDate"].dt.to_period("Q")
+    df["Revenue"] = df["Quantity"] * df["UnitPrice"]
+    total_revenue = df.groupby("Quarter")["Revenue"].sum().reset_index()
+    total_revenue.columns = ["Quarter", "Total Revenue"]
     return total_revenue
 
 revenue_df = quarterly_revenue(df)
@@ -41,8 +41,8 @@ print(revenue_df.head())
   
 
 def high_demand_products(df, top_n):
-    total_quantity = df.groupby('StockCode')['Quantity'].sum().reset_index()
-    sorted_products = total_quantity.sort_values(by='Quantity', ascending=False)
+    total_quantity = df.groupby("StockCode")["Quantity"].sum().reset_index()
+    sorted_products = total_quantity.sort_values(by="Quantity", ascending=False)
     top_products = sorted_products.head(top_n)
     return top_products
 
@@ -52,9 +52,9 @@ print(top_products_df)
 
 
 def purchase_patterns(df):
-    summary = df.groupby('Description')[['Quantity', 'UnitPrice']].mean().reset_index()
-    summary.columns = ['Description', 'avg_quantity', 'avg_unit_price']
-    summary.rename(columns={'Description': 'product'}, inplace=True)
+    summary = df.groupby("Description")[["Quantity", "UnitPrice"]].mean().reset_index()
+    summary.columns = ["Description", "avg_quantity", "avg_unit_price"]
+    summary.rename(columns={"Description": "product"}, inplace=True)
     return summary
 
 print(purchase_patterns(df))
